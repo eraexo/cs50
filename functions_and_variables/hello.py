dc4ea0389ab661932a5d736f1f1d7f2c7637eab9
Era|Exo <eraexo@eraexo.com>
2025-07-24 19:09:33 +0000

    updated hello.py

--- functions_and_variables/hello.py
+++ functions_and_variables/hello.py
@@ -4,3 +4,9 @@ print("hello world")
 # Variables and input/output
 name = input("What's your name? ")
 print("My name is ", name)
+
+# Escape characters
+name = input("What's your name? ")
+print("My name is \"", name, "\"")  # Using escape character for double quotes
+print("My name is ", name, sep="\n")  # Using sep to go to a new line
+print(f"My name is {name}")  # Using f-string for formatted output

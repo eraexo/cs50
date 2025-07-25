5b0d92c0ba2e095f107611cf931c5e32c85753bd
Era|Exo <eraexo@eraexo.com>
2025-07-25 20:13:29 +0000

    updated hello.py

--- functions_and_variables/hello.py
+++ functions_and_variables/hello.py
@@ -1,6 +1,7 @@
 # Hello world
 print("hello world")
 
+""" 
 # Variables and input/output
 name = input("What's your name? ")
 print("My name is ", name)
@@ -10,3 +11,9 @@ name = input("What's your name? ")
 print("My name is \"", name, "\"")  # Using escape character for double quotes
 print("My name is ", name, sep="\n")  # Using sep to go to a new line
 print(f"My name is {name}")  # Using f-string for formatted output
+"""
+
+# Remove whitespace
+name = input("What's your name? ")
+print("My name is", name.strip().capitalize())  # Using strip() to remove leading/trailing whitespace and capitalize() to capitalize the first letter
+

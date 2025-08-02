e14e6a88db1e27e75c9656e6187e95e156aa0baf
Era|Exo <eraexo@eraexo.com>
2025-08-02 19:05:16 +0000

    Add user input functionality to compare two numbers

---
+++ conditionals/conditionals.py
@@ -0,0 +1,13 @@
+# User input for comparing two numbers
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+
+# Conditional statements to compare two numbers    
+if (x > y):
+    print(f"{x} is greater than {y}")
+elif (x < y):
+    print(f"{x} is less than {y}")
+else:
+    print(f"{x} is equal to {y}")
+
+
